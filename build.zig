34b1a71e3cda2ff0843c4d59f593d486ac8a9366
Travis Staloch <1562827+travisstaloch@users.noreply.github.com>
2023-10-07 17:06:36 -0700

    build: expose simdjzon-rpc module

--- build.zig
+++ build.zig
@@ -4,20 +4,26 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const simdjzon_dep = b.dependency("simdjzon", .{ .target = target, .optimize = optimize });
-    const lib = b.addStaticLibrary(.{
-        .name = "simdjzon-rpc",
-        .root_source_file = .{ .path = "src/lib.zig" },
-        .target = target,
-        .optimize = optimize,
+    const simdjzon_mod = simdjzon_dep.module("simdjzon");
+    const mod = b.addModule("simdjzon-rpc", .{
+        .source_file = .{ .path = "src/lib.zig" },
+        .dependencies = &.{.{ .name = "simdjzon", .module = simdjzon_mod }},
     });
-    b.installArtifact(lib);
+    _ = mod;
+    // const lib = b.addStaticLibrary(.{
+    //     .name = "simdjzon-rpc",
+    //     .root_source_file = .{ .path = "src/lib.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // b.installArtifact(lib);
 
     const main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/lib.zig" },
         .target = target,
         .optimize = optimize,
     });
-    main_tests.addModule("simdjzon", simdjzon_dep.module("simdjzon"));
+    main_tests.addModule("simdjzon", simdjzon_mod);
     const run_main_tests = b.addRunArtifact(main_tests);
     run_main_tests.has_side_effects = true;
     const test_step = b.step("test", "Run library tests");
