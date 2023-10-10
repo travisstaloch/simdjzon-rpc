c2c2270daa8ea51240d7df6d7f731fbcfa537da6
Travis Staloch <1562827+travisstaloch@users.noreply.github.com>
2023-10-09 20:09:18 -0700

    examples: simplify, remove build.zig

--- build.zig
+++ build.zig
@@ -9,7 +9,6 @@ pub fn build(b: *std.Build) void {
         .source_file = .{ .path = "src/lib.zig" },
         .dependencies = &.{.{ .name = "simdjzon", .module = simdjzon_mod }},
     });
-    _ = mod;
 
     const main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/lib.zig" },
@@ -21,4 +20,29 @@ pub fn build(b: *std.Build) void {
     run_main_tests.has_side_effects = true;
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_main_tests.step);
+
+    try buildExample(b, target, optimize, mod, "http-echo-server");
+}
+
+fn buildExample(
+    b: *std.Build,
+    target: std.zig.CrossTarget,
+    optimize: std.builtin.OptimizeMode,
+    mod: *std.Build.Module,
+    name: []const u8,
+) !void {
+    const exe = b.addExecutable(.{
+        .name = name,
+        .root_source_file = .{
+            .path = b.fmt("examples/{s}.zig", .{name}),
+        },
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.addModule("simdjzon-rpc", mod);
+    b.installArtifact(exe);
+    const run_exe = b.addRunArtifact(exe);
+    const run_exe_step = b.step("run", b.fmt("Run {s}", .{name}));
+    run_exe_step.dependOn(&run_exe.step);
+    if (b.args) |args| run_exe.addArgs(args);
 }
