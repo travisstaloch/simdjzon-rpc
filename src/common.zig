c20fc5dd447da1786cf3c77ae702f4f8df7113ab
Travis Staloch <1562827+travisstaloch@users.noreply.github.com>
2023-10-13 17:44:18 -0700

    lib: create common.zig and move some stuff there
    prepare for a std.json implementation to come soon

---
+++ src/common.zig
@@ -0,0 +1,181 @@
+const std = @import("std");
+const mem = std.mem;
+
+pub const Error = struct {
+    code: Code,
+    note: []const u8,
+
+    pub const Code = enum(i32) {
+        parse_error = -32700,
+        invalid_request = -32600,
+        method_not_found = -32601,
+        invalid_params = -32602,
+        internal_error = -32603,
+        _, // -32000 to -32099: server_error
+    };
+
+    pub fn init(code: Code, note: []const u8) Error {
+        return .{ .code = code, .note = note };
+    }
+
+    pub fn format(err: Error, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        _ = options;
+        _ = fmt;
+        try writer.print("code={} note={s}", .{ @intFromEnum(err.code), err.note });
+    }
+};
+
+pub const Callback = fn (rpc_ptr: *anyopaque) void;
+
+pub const NamedCallback = struct {
+    name: []const u8,
+    callback: *const Callback,
+    rpc_ptr: *anyopaque = undefined,
+};
+
+pub const FindAndCall = @TypeOf(Engine.findAndCall);
+
+pub const Engine = struct {
+    callbacks: std.StringHashMapUnmanaged(NamedCallback) = .{},
+    allocator: mem.Allocator,
+
+    pub fn deinit(e: *Engine) void {
+        e.callbacks.deinit(e.allocator);
+    }
+
+    pub fn putCallback(e: *Engine, named_callback: NamedCallback) !void {
+        try e.callbacks.put(e.allocator, named_callback.name, named_callback);
+    }
+
+    fn findAndCall(engine: Engine, rpc_ptr: *anyopaque, name: []const u8) bool {
+        const named_cb = engine.callbacks.get(name) orelse return false;
+        named_cb.callback(rpc_ptr);
+        return true;
+    }
+
+    pub fn parseAndRespond(engine: Engine, rpc: anytype) !void {
+        if (rpc.parse(engine.allocator)) |err| {
+            try rpc.writeError(err);
+            try rpc.writer.flush();
+            return;
+        }
+        try rpc.startResponse();
+        if (try rpc.respond(engine, findAndCall)) |err|
+            try rpc.writeError(err);
+        try rpc.finishResponse();
+    }
+};
+
+/// some test cases from https://www.jsonrpc.org/specification
+pub const test_cases_1 = .{
+    // call with positional params
+    .{
+        \\{"jsonrpc": "2.0", "method": "subtract", "params": [42, 23], "id": 1}
+        ,
+        .{ .method = "subtract", .id = "1" },
+    },
+    .{
+        \\{"jsonrpc": "2.0", "method": "subtract", "params": [23, 42], "id": 2}
+        ,
+        .{ .method = "subtract", .id = "2" },
+    },
+    // call with named params
+    .{
+        \\{"jsonrpc": "2.0", "method": "subtract", "params": {"subtrahend": 23, "minuend": 42}, "id": 3}
+        ,
+        .{ .method = "subtract", .id = "3" },
+    },
+    .{
+        \\{"jsonrpc": "2.0", "method": "subtract", "params": {"minuend": 42, "subtrahend": 23}, "id": 4}
+        ,
+        .{ .method = "subtract", .id = "4" },
+    },
+};
+
+// from https://www.jsonrpc.org/specification#examples
+pub const test_cases_2 = [_][2][]const u8{
+    .{ // rpc call with positional parameters
+        \\{"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": 1}
+        ,
+        \\{"jsonrpc":"2.0","result":7,"id":"1"}
+    },
+    .{ // rpc call with named parameters
+        \\{"jsonrpc": "2.0", "method": "sum_named", "params": {"a": 1, "b": 2}, "id": 2}
+        ,
+        \\{"jsonrpc":"2.0","result":3,"id":"2"}
+    },
+    .{ // notifications
+        \\{"jsonrpc": "2.0", "method": "update", "params": [1,2,3,4,5]}
+        ,
+        "",
+    },
+    .{
+        \\{"jsonrpc": "2.0", "method": "foobar"}
+        ,
+        "",
+    },
+    .{ // valid rpc call Batch
+        \\[{"jsonrpc": "2.0", "method": "sum", "params": [1,2,4],  "id": 1},
+        \\ {"jsonrpc": "2.0", "method": "sum", "params": [1,2,10], "id": 2}]
+        ,
+        \\[{"jsonrpc":"2.0","result":7,"id":"1"},{"jsonrpc":"2.0","result":13,"id":"2"}]
+    },
+    .{ // rpc call of non-existent method
+        \\{"jsonrpc": "2.0", "method": "foobar", "id": "1"}
+        ,
+        \\{"jsonrpc":"2.0","error":{"code":-32601,"message":"Method not found"},"id":"1"}
+    },
+    .{ // rpc call with invalid JSON
+        \\{"jsonrpc": "2.0", "method": "foobar, "params": "bar", "baz]
+        ,
+        \\{"jsonrpc":"2.0","error":{"code":-32700,"message":"Invalid JSON was received by the server."},"id":null}
+    },
+    .{ // rpc call with invalid Request object
+        \\{"jsonrpc": "2.0", "method": 1, "params": "bar"}
+        ,
+        \\{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. 'method' field must be a string."},"id":null}
+    },
+    .{ // rpc call Batch, invalid JSON
+        \\[
+        \\  {"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": "1"},
+        \\  {"jsonrpc": "2.0", "method"
+        \\]
+        ,
+        \\{"jsonrpc":"2.0","error":{"code":-32700,"message":"Invalid JSON was received by the server."},"id":null}
+    },
+    .{ // rpc call with an empty Array
+        \\[]
+        ,
+        \\[{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. Empty array."},"id":null}]
+    },
+    .{ // rpc call with an invalid Batch (but not empty)
+        \\[1]
+        ,
+        \\[{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. Not an object."},"id":null}]
+    },
+    .{ // rpc call with invalid Batch
+        \\[1,2,3]
+        ,
+        \\[{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. Not an object."},"id":null},{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. Not an object."},"id":null},{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. Not an object."},"id":null}]
+    },
+    .{ // rpc call Batch
+        \\[
+        \\    {"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": "1"},
+        \\    {"jsonrpc": "2.0", "method": "notify_hello", "params": [7]},
+        \\    {"jsonrpc": "2.0", "method": "subtract", "params": [42,23], "id": "2"},
+        \\    {"foo": "boo"},
+        \\    {"jsonrpc": "2.0", "method": "foo.get", "params": {"name": "myself"}, "id": "5"},
+        \\    {"jsonrpc": "2.0", "method": "get_data", "id": "9"} 
+        \\]
+        ,
+        \\[{"jsonrpc":"2.0","result":7,"id":"1"},{"jsonrpc":"2.0","result":19,"id":"2"},{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. Missing 'jsonrpc' field."},"id":null},{"jsonrpc":"2.0","error":{"code":-32601,"message":"Method not found"},"id":"5"},{"jsonrpc":"2.0","result":["hello",5],"id":"9"}]
+    },
+    .{ // rpc call Batch (all notifications)
+        \\[
+        \\    {"jsonrpc": "2.0", "method": "notify_sum", "params": [1,2,4]},
+        \\    {"jsonrpc": "2.0", "method": "notify_hello", "params": [7]}
+        \\]
+        ,
+        "",
+    },
+};
--- src/lib.zig
+++ src/lib.zig
@@ -4,38 +4,17 @@ const talloc = testing.allocator;
 const mem = std.mem;
 const simdjzon = @import("simdjzon");
 const dom = simdjzon.dom;
+const common = @import("common.zig");
 
-pub const Error = struct {
-    code: Code,
-    note: []const u8,
-
-    pub const Code = enum(i32) {
-        parse_error = -32700,
-        invalid_request = -32600,
-        method_not_found = -32601,
-        invalid_params = -32602,
-        internal_error = -32603,
-        _, // -32000 to -32099: server_error
-    };
-
-    pub fn init(code: Code, note: []const u8) Error {
-        return .{ .code = code, .note = note };
-    }
-
-    pub fn format(err: Error, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
-        _ = options;
-        _ = fmt;
-        try writer.print("code={} note={s}", .{ @intFromEnum(err.code), err.note });
-    }
-};
+pub const Error = common.Error;
 
 pub const RpcInfo = struct {
     id_buf: [25]u8 = undefined,
     id: []const u8,
-    method_name: []const u8,
+    method: []const u8,
     element: dom.Element = undefined,
 
-    pub const empty = RpcInfo{ .id = "", .method_name = "" };
+    pub const empty = RpcInfo{ .id = "", .method = "" };
 
     // custom json parsing method
     pub fn jsonParse(doc: dom.Element, out: anytype, _: simdjzon.common.GetOptions) !void {
@@ -89,9 +68,9 @@ pub const RpcInfo = struct {
                 "Invalid Request. 'params' field must be an array or object.",
             );
 
-        out.method_name = try method.get_string();
+        out.method = try method.get_string();
         out.element = doc;
-        // std.debug.print("method_name={s} id={s}\n", .{ out.method_name, out.id });
+        // std.debug.print("method={s} id={s}\n", .{ out.method, out.id });
         return null;
     }
 };
@@ -112,31 +91,8 @@ fn checkField(
 }
 
 test RpcInfo {
-    // some test cases from https://www.jsonrpc.org/specification
-    const input_expecteds = [_]struct { []const u8, RpcInfo }{
-        // call with positional params
-        .{
-            \\{"jsonrpc": "2.0", "method": "subtract", "params": [42, 23], "id": 1}
-            ,
-            .{ .method_name = "subtract", .id = "1" },
-        },
-        .{
-            \\{"jsonrpc": "2.0", "method": "subtract", "params": [23, 42], "id": 2}
-            ,
-            .{ .method_name = "subtract", .id = "2" },
-        },
-        // call with named params
-        .{
-            \\{"jsonrpc": "2.0", "method": "subtract", "params": {"subtrahend": 23, "minuend": 42}, "id": 3}
-            ,
-            .{ .method_name = "subtract", .id = "3" },
-        },
-        .{
-            \\{"jsonrpc": "2.0", "method": "subtract", "params": {"minuend": 42, "subtrahend": 23}, "id": 4}
-            ,
-            .{ .method_name = "subtract", .id = "4" },
-        },
-    };
+    const input_expecteds: []const struct { []const u8, RpcInfo } =
+        &common.test_cases_1;
 
     for (input_expecteds) |ie| {
         const input, const expected = ie;
@@ -148,7 +104,7 @@ test RpcInfo {
         // std.debug.print("input={s}\n", .{input});
         try parser.element().get(&actual);
         try checkField("id", expected, actual, input);
-        try checkField("method_name", expected, actual, input);
+        try checkField("method", expected, actual, input);
     }
 }
 
@@ -202,11 +158,11 @@ fn RpcImpl(comptime R: type, comptime W: type) type {
             self.parser.deinit();
         }
 
-        fn startResponse(self: *Self) !void {
+        pub fn startResponse(self: *Self) !void {
             if (self.elements == .array) try self.writer.writer().writeByte('[');
         }
 
-        fn finishResponse(self: *Self) !void {
+        pub fn finishResponse(self: *Self) !void {
             if (self.elements == .array) {
                 // instead of writing an empty array, skip flush so we don't
                 // write anything
@@ -293,12 +249,10 @@ fn RpcImpl(comptime R: type, comptime W: type) type {
             return JsonValue.init(arr.at(index) orelse return null);
         }
 
-        const FindAndCall = @TypeOf(Engine.findAndCall);
-
         pub fn respond(
             self: *Self,
-            engine: Engine,
-            find_and_call: *const FindAndCall,
+            engine: common.Engine,
+            find_and_call: *const common.FindAndCall,
         ) !?Error {
             switch (self.elements) {
                 .array => |array| {
@@ -317,7 +271,7 @@ fn RpcImpl(comptime R: type, comptime W: type) type {
                         if (!find_and_call(
                             engine,
                             self,
-                            self.info.method_name,
+                            self.info.method,
                         )) {
                             if (self.info.id.len != 0)
                                 try self.writeError(Error.init(.method_not_found, "Method not found"));
@@ -333,7 +287,7 @@ fn RpcImpl(comptime R: type, comptime W: type) type {
                     if (!find_and_call(
                         engine,
                         self,
-                        self.info.method_name,
+                        self.info.method,
                     )) {
                         if (self.info.id.len != 0)
                             return Error.init(.method_not_found, "Method not found");
@@ -397,46 +351,7 @@ test "indexed params" {
     try testing.expect(rpc.getParamByIndex(2) == null);
 }
 
-pub const Callback = fn (rpc_ptr: *anyopaque) void;
-
-pub const NamedCallback = struct {
-    name: []const u8,
-    callback: *const Callback,
-    rpc_ptr: *anyopaque = undefined,
-};
-
-pub const Engine = struct {
-    callbacks: std.StringHashMapUnmanaged(NamedCallback) = .{},
-    allocator: mem.Allocator,
-
-    pub fn deinit(e: *Engine) void {
-        e.callbacks.deinit(e.allocator);
-    }
-
-    pub fn putCallback(e: *Engine, named_callback: NamedCallback) !void {
-        try e.callbacks.put(e.allocator, named_callback.name, named_callback);
-    }
-
-    fn findAndCall(engine: Engine, rpc_ptr: *anyopaque, name: []const u8) bool {
-        const named_cb = engine.callbacks.get(name) orelse return false;
-        named_cb.callback(rpc_ptr);
-        return true;
-    }
-
-    pub fn parseAndRespond(engine: Engine, rpc: anytype) !void {
-        if (rpc.parse(engine.allocator)) |err| {
-            try rpc.writeError(err);
-            try rpc.writer.flush();
-            return;
-        }
-        try rpc.startResponse();
-        if (try rpc.respond(engine, findAndCall)) |err|
-            try rpc.writeError(err);
-        try rpc.finishResponse();
-    }
-};
-
-/// exposes user facing type erased methods
+// /// exposes user facing type erased methods
 pub fn Rpc(comptime R: type, comptime W: type) type {
     return struct {
         pub const TypedRpc = RpcImpl(R, W);
@@ -485,7 +400,7 @@ pub fn Rpc(comptime R: type, comptime W: type) type {
 }
 
 test {
-    var e = Engine{ .allocator = talloc };
+    var e = common.Engine{ .allocator = talloc };
     defer e.deinit();
 
     try e.putCallback(.{
@@ -539,95 +454,7 @@ test {
         }.func,
     });
 
-    // from https://www.jsonrpc.org/specification#examples
-    const input_expecteds = [_][2][]const u8{
-        .{ // rpc call with positional parameters
-            \\{"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": 1}
-            ,
-            \\{"jsonrpc":"2.0","result":7,"id":"1"}
-        },
-        .{ // rpc call with named parameters
-            \\{"jsonrpc": "2.0", "method": "sum_named", "params": {"a": 1, "b": 2}, "id": 2}
-            ,
-            \\{"jsonrpc":"2.0","result":3,"id":"2"}
-        },
-        .{ // notifications
-            \\{"jsonrpc": "2.0", "method": "update", "params": [1,2,3,4,5]}
-            ,
-            "",
-        },
-        .{
-            \\{"jsonrpc": "2.0", "method": "foobar"}
-            ,
-            "",
-        },
-        .{ // valid rpc call Batch
-            \\[{"jsonrpc": "2.0", "method": "sum", "params": [1,2,4],  "id": 1},
-            \\ {"jsonrpc": "2.0", "method": "sum", "params": [1,2,10], "id": 2}]
-            ,
-            \\[{"jsonrpc":"2.0","result":7,"id":"1"},{"jsonrpc":"2.0","result":13,"id":"2"}]
-        },
-        .{ // rpc call of non-existent method
-            \\{"jsonrpc": "2.0", "method": "foobar", "id": "1"}
-            ,
-            \\{"jsonrpc":"2.0","error":{"code":-32601,"message":"Method not found"},"id":"1"}
-        },
-        .{ // rpc call with invalid JSON
-            \\{"jsonrpc": "2.0", "method": "foobar, "params": "bar", "baz]
-            ,
-            \\{"jsonrpc":"2.0","error":{"code":-32700,"message":"Invalid JSON was received by the server."},"id":null}
-        },
-        .{ // rpc call with invalid Request object
-            \\{"jsonrpc": "2.0", "method": 1, "params": "bar"}
-            ,
-            \\{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. 'method' field must be a string."},"id":null}
-        },
-        .{ // rpc call Batch, invalid JSON
-            \\[
-            \\  {"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": "1"},
-            \\  {"jsonrpc": "2.0", "method"
-            \\]
-            ,
-            \\{"jsonrpc":"2.0","error":{"code":-32700,"message":"Invalid JSON was received by the server."},"id":null}
-        },
-        .{ // rpc call with an empty Array
-            \\[]
-            ,
-            \\[{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. Empty array."},"id":null}]
-        },
-        .{ // rpc call with an invalid Batch (but not empty)
-            \\[1]
-            ,
-            \\[{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. Not an object."},"id":null}]
-        },
-        .{ // rpc call with invalid Batch
-            \\[1,2,3]
-            ,
-            \\[{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. Not an object."},"id":null},{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. Not an object."},"id":null},{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. Not an object."},"id":null}]
-        },
-        .{ // rpc call Batch
-            \\[
-            \\    {"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": "1"},
-            \\    {"jsonrpc": "2.0", "method": "notify_hello", "params": [7]},
-            \\    {"jsonrpc": "2.0", "method": "subtract", "params": [42,23], "id": "2"},
-            \\    {"foo": "boo"},
-            \\    {"jsonrpc": "2.0", "method": "foo.get", "params": {"name": "myself"}, "id": "5"},
-            \\    {"jsonrpc": "2.0", "method": "get_data", "id": "9"} 
-            \\]
-            ,
-            \\[{"jsonrpc":"2.0","result":7,"id":"1"},{"jsonrpc":"2.0","result":19,"id":"2"},{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request. Missing 'jsonrpc' field."},"id":null},{"jsonrpc":"2.0","error":{"code":-32601,"message":"Method not found"},"id":"5"},{"jsonrpc":"2.0","result":["hello",5],"id":"9"}]
-        },
-        .{ // rpc call Batch (all notifications)
-            \\[
-            \\    {"jsonrpc": "2.0", "method": "notify_sum", "params": [1,2,4]},
-            \\    {"jsonrpc": "2.0", "method": "notify_hello", "params": [7]}
-            \\]
-            ,
-            "",
-        },
-    };
-
-    for (input_expecteds) |ie| {
+    for (common.test_cases_2) |ie| {
         const input, const expected = ie;
         var input_fbs = std.io.fixedBufferStream(input);
         var buf: [512]u8 = undefined;
